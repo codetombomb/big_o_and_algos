790bef41f907a1c381381fbc64a588f208f702bb
Tom Tobar <codetombomb@gmail.com>
2020-10-27 12:54:41 -0500

    Added get function to SLL and numbered the SLL files to denote their order

---
+++ dataStructures/SinglyLinkedList/(5)get.js
@@ -0,0 +1,103 @@
+//                           GET()
+
+//********************** PSEUDO CODE ************************
+
+//- This function should accept an index
+
+//- If the index is less than zero or greater than or 
+// equal to the length of the list, return null
+
+//- Create a counter variable that will count up to the
+// value of the index passed into the function
+
+//- Create a variable that will keep track of the current
+// node 
+
+//- While the counter is not equal to the index passes 
+// into the function, set the current variable tracker 
+// to the next node and increment the counter by one
+
+//- Return the node at that specific index using the
+// current variable
+
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+
+    shift() {
+        if (this.length === 0) return undefined;
+        let oldHead = this.head;
+        this.head = oldHead.next;
+        this.length--;
+        if (this.length === 0) this.tail = null;
+        return oldHead;
+    }
+
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    get(idx) {
+        if (idx < 0 || idx >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+}
+
+const list = new SinglyLinkedList();
