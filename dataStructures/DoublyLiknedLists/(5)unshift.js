4b502b0b67bf1333a30bae3aaa312cd27f8932a5
Tom Tobar <codetombomb@gmail.com>
2020-10-28 13:04:27 -0500

    Added notes for unshift() on DLL

---
+++ dataStructures/DoublyLiknedLists/(5)unshift.js
@@ -0,0 +1,97 @@
+//                          UNSHIFT()
+
+// - Adding a node to the beginning of the Doubly Linked List
+
+//************************ UNSHIFT PSEUDO CODE ************************
+
+// - Create a new node with the value passed to the function 
+// - If the length is 0:
+'   '//- Set the head to be the new node
+'   '//- Set the tail to be the new node
+// - Otherwise:
+'   '//- Set the prev property on the head of the list to be the new 
+'   '//node
+'   '//- Set the next property on the new node to be the head property
+'   '//- Update the head to be the new node
+// - Increment the length
+// - Return the list
+
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+class DoublyLinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        let oldTail = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = oldTail.prev;
+            this.tail.next = null;
+            oldTail.prev = null;
+        }
+        this.length--;
+        return oldTail;
+    }
+
+    shift() {
+        if (!this.head) return undefined;
+        let oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+    //UNSHIFT
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let list = new DoublyLinkedList();
+list.push('one');
+list.push(2);
+list.push('three');
+list.push(4);
